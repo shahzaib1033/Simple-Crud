a83d7e81d6441d0593c508bbd4ed97ac2373ff76
M Shahzaib <shahzaibboota65@gmail.com>
2024-03-28 15:44:23 +0500

    handle the cors

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Employee = require('./Employee');
+const cors = require('cors');
 require('dotenv').config()
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.DATABASE)
